f37db8120f5d0d3059cdfc7cf30b91796695784c
Radeonares32 <radeonares@gmail.com>
2021-04-24 15:09:40 +0300

    new route

--- routes/movie.js
+++ routes/movie.js
@@ -19,6 +19,11 @@ router.post('/', function(req, res, next) {
   });
   //res.json(data);
 });
+//top 10 list
+router.get("/top10",(req,res)=>{
+   const top10 = movie.find({ }).limit(10).sort({imdb_score:-1});
+   top10.then(data=>res.json(data)).catch(err=>res.json(err));
+});
 router.get("/:movie_id",(req,res,next)=>{
   const findById = movie.findById(req.params.movie_id);
 
@@ -31,8 +36,19 @@ router.get("/:movie_id",(req,res,next)=>{
   }).
   catch(err=>next(err));
 });
+
 router.put("/:movie_id",(req,res)=>{
-  const update = movie.findByIdAndUpdate(req.params.movie_id,req.body);
+  const update = movie.findByIdAndUpdate(req.params.movie_id,req.body,{new:true,useFindAndModify:false});
   update.then(data=>res.json(data)).catch(err=>res.json(err));
+});
+router.delete("/:movie_id",(req,res)=>{
+  const deleted = movie.findByIdAndRemove(req.params.movie_id,{useFindAndModify:false});
+  deleted.then(data=>res.json(data)).catch(err=>res.json(err));
+});
+router.get("/between/:start_year/:end_year",(req,res)=>{
+  const between = movie.find({
+    year:{"$gte": parseInt(req.params.start_year),"$lte":parseInt(req.params.end_year)}
+  });
+  between.then(data=>res.json(data)).catch(err=>res.json(err));
 });
 module.exports = router;
